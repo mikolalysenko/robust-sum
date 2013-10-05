d45543f44b0582a8de30ee52ab01ac51fd4a647c
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 18:08:24 -0500

    updated test case

--- test/test.js
+++ test/test.js
@@ -9,5 +9,10 @@ require("tape")(function(t) {
 	t.same(robustSum([1, 1e64], [1e-64, 2]), [1e-64, 3, 1e64])
   
   t.same(robustSum([0], [1], [1,1,1]), [0,0,1])
+  
+  for(var i=-10; i<=10; ++i)
+  for(var j=-10; j<=10; ++j) {
+    t.same(robustSum([i], [j]), [i+j])
+  }
 	t.end()
 })
